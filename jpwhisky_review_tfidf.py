0b13f8d27e0a769ba20425a0d1c6ae3cf2e4e056
Jacques Sham <jjsham@dons.usfca.edu>
2019-06-08 18:01:10 -0700

    Improve display feature and upload viz

--- jpwhisky_review_tfidf.py
+++ jpwhisky_review_tfidf.py
@@ -7,19 +7,23 @@ from sklearn.feature_extraction import stop_words
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 
+# Preprocess the comment to remove punctuation and split into a list
+def clean_words(post):
+    regex = re.compile('[' + re.escape(string.punctuation) + ' 0-9\\r\\t\\n]')
+    sentence = regex.sub(' ', post.lower())
+    return sentence.split(' ')
+
 # Define tokenizer for Tfidf model
 def tokenizer(post):
-    regex = re.compile('[' + re.escape(string.punctuation) + ' 0-9\\r\\t\\n]')
+    temp_list = clean_words(post)
     stemmer = PorterStemmer()
-    sentence = regex.sub(' ', post.lower())
-    temp_list = sentence.split(' ')
     temp_list = [stemmer.stem(word) for word in temp_list if 
                  word != '' and word not in stop_words.ENGLISH_STOP_WORDS]
     return temp_list
 
 # Read file and extract the comments and convert to np array
 def read_reviews(filename, colname):
-    jp_whisky = pd.read_csv(filename)
+    jp_whisky = pd.read_csv(filename, encoding = 'ISO-8859-1')
     comments_list = jp_whisky[colname].tolist()
     return jp_whisky, comments_list
 
@@ -41,3 +45,18 @@ def tfidf_fit_trans(comments_list):
 def get_results(features, scores, article_index, top_n):
     index = np.argsort(scores[article_index])[::-1]
     return features[index[:top_n]], scores[article_index, index[:top_n]]
+
+def get_words(comment):
+    temp_list = clean_words(comment)
+    stemmer = PorterStemmer()
+    word_dict = {}
+    for word in temp_list:
+        word_stemmed = stemmer.stem(word)
+        if word != '' and word not in stop_words.ENGLISH_STOP_WORDS \
+           and word_stemmed not in word_dict:
+           word_dict[word_stemmed] = [word]
+        elif word != '' and word not in stop_words.ENGLISH_STOP_WORDS \
+           and word_stemmed in word_dict:
+           word_dict[word_stemmed] = word_dict[word_stemmed].append(word)
+    return word_dict
+
